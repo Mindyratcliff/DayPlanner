c585bac534acf5b1856020e51126439624972fb4
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-11-10 21:29:39 -0500

    Added on-click event for save button with debugging. Added keypress event for text

--- script.js
+++ script.js
@@ -7,17 +7,29 @@ var now = moment().format("dddd, MMMM Do YYYY");
 currentDayEl.text(now);
 
 //WHEN I scroll down
-//THEN I am presented with time blocks for standard business hours
-
-var timeBlockEl = $("#time-block");
+//THEN I am presented with time blocks for standard business hour
+var currentHour = moment().hour();
+var hourEls = [];
 
 
 //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
 //WHEN I click into a time block
 //THEN I can enter an event
+var textEntry = $(".form-control");
+textEntry.on("keypress", function (writeEvent){
+    writeEvent.preventDefault();
+    
+})
 //WHEN I click the save button for that time block
-//THEN the text for that event is saved in local storage
+var saveButton = $(".saveBtn");
+saveButton.on("click", function (saveEvent) {
+    saveEvent.preventDefault();
+    //THEN the text for that event is saved in local storage
+    localStorage.setItem("time", textValue);
+    console.log("I clicked it");
+}
+)
 //WHEN I refresh the page
 //THEN the saved events persist
 
