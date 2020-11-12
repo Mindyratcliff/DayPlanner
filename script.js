b033b140ba28539a9e07780363a170fe6d34dc3a
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-11-11 20:08:08 -0500

    Added colorCoded function and if statements to set up conditionals for adding classes

--- script.js
+++ script.js
@@ -9,17 +9,43 @@ currentDayEl.text(now);
 //WHEN I scroll down
 //THEN I am presented with time blocks for standard business hour
 var currentHour = moment().hour();
+console.log(currentHour);
 var hourEls = [];
 
+var hours = $(".hourEl");
+ hours.each(function(){
+     hourEls.push($(this).attr("value"));                      
+ });
 
-//WHEN I view the time blocks for that day
+ var hourBlocks = $(".hour");
+ 
+
+ //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+function colorCoded (){
+    hourEls = parseInt(hourEls);
+    console.log(hourEls);
+    if (currentHour < hourEls[hourDiv]){
+        hourBlocks.addClass(".past");
+    }
+    if (currentHour == hourEls[hourDiv]){
+        hourBlocks.addClass(".present");
+    }
+    if (currentHour > hourEls[hourDiv]){
+        hourBlocks.addClass(".future");
+    }
+}
+
+hourEls.forEach(colorCoded);
+  
+
 //WHEN I click into a time block
 //THEN I can enter an event
 var textEntry = $(".form-control");
 textEntry.on("keypress", function (writeEvent){
     writeEvent.preventDefault();
-    
+
 })
 //WHEN I click the save button for that time block
 var saveButton = $(".saveBtn");
