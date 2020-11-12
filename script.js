dc8f95de9d69d530617db76c406e2de48b9199b2
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-11-12 16:25:53 -0500

    Function displaying colors for hours is fully functional.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // JavaScript function that wraps everything
-//$(document).ready(function() {
+$(document).ready(function() {
 
 //WHEN I open the planner the current day is displayed at the top of the calendar
 var currentDayEl = $("#currentDay");
@@ -9,32 +9,31 @@ currentDayEl.text(now);
 //WHEN I scroll down
 //THEN I am presented with time blocks for standard business hour
 var currentHour = moment().hour();
-console.log(currentHour);
 var hourEls = [];
 
 var hours = $(".hourEl");
  hours.each(function(){
      hourEls.push($(this).attr("value"));                      
  });
-
- var hourBlocks = $(".hour");
+ 
  
 
  //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
 function colorCoded (){
-    hourEls = parseInt(hourEls);
-    console.log(hourEls);
-    if (currentHour < hourEls[hourDiv]){
-        hourBlocks.addClass(".past");
+    var intHourEls = hourEls.splice(',').map(Number);
+    console.log(intHourEls);
+    console.log(currentHour);
+    for (var i = 0; i < intHourEls.length; i++){
+    if (currentHour > intHourEls[i]){
+        $("div[value='"+ intHourEls[i] +"']").addClass("past");
+    } else if (currentHour === intHourEls[i]){
+        $("div[value='"+ intHourEls[i] +"']").addClass("present");
+    } else {
+        $("div[value='"+ intHourEls[i] +"']").addClass("future");
     }
-    if (currentHour == hourEls[hourDiv]){
-        hourBlocks.addClass(".present");
-    }
-    if (currentHour > hourEls[hourDiv]){
-        hourBlocks.addClass(".future");
-    }
+}
 }
 
 hourEls.forEach(colorCoded);
@@ -42,20 +41,32 @@ hourEls.forEach(colorCoded);
 
 //WHEN I click into a time block
 //THEN I can enter an event
-var textEntry = $(".form-control");
-textEntry.on("keypress", function (writeEvent){
-    writeEvent.preventDefault();
+     
 
-})
 //WHEN I click the save button for that time block
 var saveButton = $(".saveBtn");
-saveButton.on("click", function (saveEvent) {
-    saveEvent.preventDefault();
-    //THEN the text for that event is saved in local storage
-    localStorage.setItem("time", textValue);
-    console.log("I clicked it");
+saveButton.on("click", saveValue);
+
+
+function saveValue(e){
+    var textEntry = $(".form-control").text;  
+    localStorage.setItem("id", textEntry); 
 }
-)
+
 //WHEN I refresh the page
 //THEN the saved events persist
+//get the saved value function - return the value of "v" from localStorage. 
+function getSavedValue  (v) {
+    if (!localStorage.getItem(v)) {
+        return "";// You can change this to your defualt value. 
+    }
+    return localStorage.getItem(v);
+}
+getSavedValue();
 
+})
+
+
+
+
+        
